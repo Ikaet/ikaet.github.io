90fd54b663c126425c56b5a5222895703f09a731
Ikaet <anibalag_ing.mec@icloud.com>
2023-11-14 07:41:03 +0100

    change imgs public to asset folder and right directory -- update basePath variable (deleted) -- moving favico but not used in the new folder -- created routes home and projects -- organized folder for styles moving globals.css

---
+++ src/app/home/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <h1> Hola Mundo Landing </h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure, 
+            repellat id maxime suscipit aliquam, quos omnis inventore asperiores 
+            sint ducimus voluptates voluptatum at neque sunt voluptate odio blanditiis 
+            libero temporibus.
+        </p>
+    </main>
+  )
+}
---
+++ src/app/projects/page.tsx
@@ -0,0 +1,13 @@
+export default function Projects() {
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+          <h1> Hola Projects</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure, 
+              repellat id maxime suscipit aliquam, quos omnis inventore asperiores 
+              sint ducimus voluptates voluptatum at neque sunt voluptate odio blanditiis 
+              libero temporibus.
+          </p>
+      </main>
+    )
+  }
+  
