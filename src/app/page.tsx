b6c874ec90000bd83b72c8493847e7d0973f2e5c
Ikaet <anibalag_ing.mec@icloud.com>
2023-11-15 08:18:40 +0100

    testing links, components, routes, tailwind vars, image rendering, access to assets, etc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
-const imgsPath = "/assets/img"
+import aagLogo from "../assets/img/logo-ag.svg"
+import Link from "next/link"
 
 import Image from 'next/image'
 
 export default function RootPage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+      <div className="z-10 max-w-5xl w-full items-center justify-between text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started NEXT Anibal Atahualpa Guerrero &nbsp;
           <code className="font-mono font-bold">src/app/page.tsx</code>
@@ -19,7 +20,7 @@ export default function RootPage() {
           >
             By{' '}
             <Image
-              src={`${imgsPath}/vercel.svg`}
+              src="/vercel.svg"
               alt="Vercel Logo"
               className="dark:invert"
               width={100}
@@ -28,7 +29,7 @@ export default function RootPage() {
               style={{ width: 100, height: 24}}
             />
             <Image
-              src={`${imgsPath}/next.svg`}
+              src="/next.svg"
               alt="Next.js Logo"
               className="dark:invert"
               width={100}
@@ -43,7 +44,7 @@ export default function RootPage() {
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70]"
-          src={`${imgsPath}/logo-ag.svg`}
+          src={aagLogo}
           alt="AAG Logo"
           width={180}
           height={37}
@@ -53,39 +54,35 @@ export default function RootPage() {
       </div>
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+        <Link 
+          href={"/home"} 
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
+            Home{' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+            Testing navigation using nextjs
           </p>
-        </a>
+        </Link>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+        <Link 
+          href={"/projects"} 
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
+            Projects{' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
+            Testing navigation to projects using next
           </p>
-        </a>
+        </Link>
 
         <a
           href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,12 +1,40 @@
+import Image from 'next/image'
+import Nav from "@/components/nav"
+import urosArcFlow from "../../assets/img/UROS-Architecture-Flow.png"
+import urosArc from "../../assets/img/UROS-Architecture.svg"
+
 export default function Projects() {
     return (
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
-          <h1> Hola Projects</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure, 
-              repellat id maxime suscipit aliquam, quos omnis inventore asperiores 
-              sint ducimus voluptates voluptatum at neque sunt voluptate odio blanditiis 
-              libero temporibus.
-          </p>
+          <Nav  />
+          <div>
+            <h1 className="text-5xl font-bold p-10"> Hola Projects</h1>
+            <p className="text-2xl">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure, 
+                repellat id maxime suscipit aliquam, quos omnis inventore asperiores 
+                sint ducimus voluptates voluptatum at neque sunt voluptate odio blanditiis 
+                libero temporibus.
+            </p>
+            <div className='flex justify-center'>
+              <Image
+                className="relative"
+                src={urosArc}
+                alt="UROS Architecture"
+                width={512}
+                height={512}
+                priority
+                style={{width: 512, height: 512}}
+              />
+              <Image
+                className="relative"
+                src={urosArcFlow}
+                alt="UROS Architecture Flow"
+                width={512}
+                height={512}
+                priority
+                style={{width: 512, height: 512}}
+              />
+            </div>
+          </div>
       </main>
     )
   }
